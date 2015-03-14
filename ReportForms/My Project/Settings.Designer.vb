19751cea49f816df996e2ef14c6d53f2af6141b3
Chris McGinn <chris.s.mcginn@live.com>
2015-03-14 13:35:01 -0400

    wip

--- ReportForms/My Project/Settings.Designer.vb
+++ ReportForms/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
 
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")> _
-        Friend ReadOnly Property Settings() As Global.MyReportForms.My.MySettings
+        Friend ReadOnly Property Settings() As Global.ReportForms.My.MySettings
             Get
-                Return Global.MyReportForms.My.MySettings.Default
+                Return Global.ReportForms.My.MySettings.Default
             End Get
         End Property
     End Module
