239e44c3397557b58f161feac4e67274b1dd7021
Chris McGinn <chris.s.mcginn@live.com>
2015-03-14 13:33:44 -0400

    remove reports

--- ReportForms/Form1.Designer.vb
+++ ReportForms/Form1.Designer.vb
@@ -22,9 +22,34 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.CrystalReportViewer1 = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
+        Me.Benefits_Domestic_Partner_Benefits1 = New MyReportForms.Benefits_Domestic_Partner_Benefits()
+        Me.SuspendLayout()
+        '
+        'CrystalReportViewer1
+        '
+        Me.CrystalReportViewer1.ActiveViewIndex = 0
+        Me.CrystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.CrystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.CrystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.CrystalReportViewer1.Location = New System.Drawing.Point(0, 0)
+        Me.CrystalReportViewer1.Name = "CrystalReportViewer1"
+        Me.CrystalReportViewer1.ReportSource = Me.Benefits_Domestic_Partner_Benefits1
+        Me.CrystalReportViewer1.Size = New System.Drawing.Size(1054, 762)
+        Me.CrystalReportViewer1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1054, 762)
+        Me.Controls.Add(Me.CrystalReportViewer1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents CrystalReportViewer1 As CrystalDecisions.Windows.Forms.CrystalReportViewer
+    Friend WithEvents Benefits_Domestic_Partner_Benefits1 As MyReportForms.Benefits_Domestic_Partner_Benefits
 
 End Class
--- ReportForms/My Project/Application.Designer.vb
+++ ReportForms/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ReportForms.Form1
+            Me.MainForm = Global.MyReportForms.Form1
         End Sub
     End Class
 End Namespace
